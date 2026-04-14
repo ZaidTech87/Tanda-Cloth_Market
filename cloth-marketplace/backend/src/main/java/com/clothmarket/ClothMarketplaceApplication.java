7b3e19cdb0499f23c7a1e300d15dc0fa87ba0aaa
Mohd Zaid <zaidalig1234@gmail.com>
2026-04-14 16:42:53 +0530

    second

--- cloth-marketplace/backend/src/main/java/com/clothmarket/ClothMarketplaceApplication.java
+++ cloth-marketplace/backend/src/main/java/com/clothmarket/ClothMarketplaceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClothMarketplaceApplication {
+public class ClothMarketplaceApplication{
     
     public static void main(String[] args) {
         SpringApplication.run(ClothMarketplaceApplication.class, args);
